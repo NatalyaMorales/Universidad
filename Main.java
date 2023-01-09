401a40e60eae91bfbcd889e66c29c2fa2ab529df
NatalyaMorales <122294898+NatalyaMorales@users.noreply.github.com>
2023-01-09 10:10:05 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+/*Natalya Patricia Morales De la Vega 
+Creado el 09 de enero de 2023
+Primer Proyecto: Hello World*/
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
